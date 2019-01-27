ed70f4f7538f8ca5ddf21f4e36d0b7121ff55225
talentamaulana10 <tatamf.id@gmail.com>
2019-01-27 13:50:31 +0700

    sizes button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import  Button  from '../component/button/Floating Action Buttons/FloatingActionButtons2';
+import  Button  from '../component/button/Sizes/sizes';
 
 export default class Home extends Component{
     render(){
