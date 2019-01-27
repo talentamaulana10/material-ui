12390b265258ef0d0b7651ff7515be5c4a2a9b73
talentamaulana10 <tatamf.id@gmail.com>
2019-01-27 13:36:20 +0700

    contained button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react'
-import BottomNavigation from '../component/bottom navigation/Bottom Navigation with no label/BottomNavigationWithNoLabel'
+import  Button  from '../component/button/Contained Buttons/containedButtons';
 
 export default class Home extends Component{
     render(){
         return(
             <div>
-                <BottomNavigation/>
+                <Button/>
             </div>
         )
     }
