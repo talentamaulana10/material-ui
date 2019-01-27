deeaf160f244540b83b3a8226f1c70bf405ddf27
talentamaulana10 <tatamf.id@gmail.com>
2019-01-27 15:04:20 +0700

    optional dialog

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import Dialog from '../component/Dialogs//Customized dialog/Customizeddialog'
+import Dialog from '../component/Dialogs/Optional sizes/OptionalSizes'
 export default class Home extends Component{
     render(){
         return(
