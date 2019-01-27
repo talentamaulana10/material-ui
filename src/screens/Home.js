797daa406fc6dbdb65696a39a77299624cc6a451
talentamaulana10 <tatamf.id@gmail.com>
2019-01-27 13:03:20 +0700

    image vatar

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react'
-import Autocmplate from '../component/autocomplate/react-autogest/react-autogest'
+import Avatar from '../component/avatar/image avatar/imageAvatar'
 
 export default class Home extends Component{
     render(){
         return(
             <div>
-                <Autocmplate/>
+                <Avatar/>
             </div>
         )
     }
