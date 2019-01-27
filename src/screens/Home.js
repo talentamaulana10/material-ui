162584720c87e5df49eca38aa2609fe67db4cfd6
talentamaulana10 <tatamf.id@gmail.com>
2019-01-27 11:50:54 +0700

    App Bar with buttons

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import Appbar from '../appbar/appBarButtons/AppBarButton'
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div>
+                <Appbar/>
+            </div>
+        )
+    }
+}
