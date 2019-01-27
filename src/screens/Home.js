bee28b5648e170873ea8aec907ca6153a64ef243
talentamaulana10 <tatamf.id@gmail.com>
2019-01-27 17:40:05 +0700

    Responsive full-screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import Dialog from '../component/Dialogs/Optional sizes/OptionalSizes'
+import Dialog from '../component/Dialogs/Responsive full-screen/Responsivefull-screen'
 export default class Home extends Component{
     render(){
         return(
