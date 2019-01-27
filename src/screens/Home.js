4499720c11dbc087acb17f8b455946b86d45ee8b
talentamaulana10 <tatamf.id@gmail.com>
2019-01-27 13:26:24 +0700

    Bottom Navigation

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,12 @@
 import React,{Component} from 'react'
 import Badges from '../component/badges/Customized Badge/customizedBadge'
+import BottomNavigation from '../component/bottom navigation/Bottom Navigation/bottomNavigation'
 
 export default class Home extends Component{
     render(){
         return(
             <div>
-                <Badges/>
+                <BottomNavigation/>
             </div>
         )
     }
